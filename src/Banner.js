ad5907bff8a5b41260e89c1c8ae4e45b02aa29bb
fullstack-developer23 <ahmadsafeer1997gmail.com>
2023-12-08 20:12:31 +0000

    fixed some issues

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-// import Banner from './catbanner.jpg';
-=======
-import banner from './catBanner.jpg';
->>>>>>> 914e2571a0b49dbd9d24ef84724b6b449f29d649
-
 
 const Banner = () => {
     return (
